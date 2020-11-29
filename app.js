cb2d3477f0a5729da279942c50dbfd9c05f79aa7
Inzikhan <inzkhan@gmail.com>
2020-11-29 17:39:05 +0500

    fixed searching

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ addBtn.addEventListener("click",function(e){
         let html = "";
         notesObj.forEach(function(element,index) {
             html += `
-            <div class="my-2 mx-2 card" style="width: 18rem;">
+            <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
                      <div class="card-body">
                         <h5 class="card-title">Note ${index + 1}</h5>
                         <p class="card-text"> ${element}</p>
